6e4adf793e2de62e85885807d130de5a0d049b3a
Ignacio Castro Manjarrez <ignacio.cm84@gmail.com>
2023-09-26 10:36:39 -0600

    Proyecto terminado.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  API_URL:`https://localhost:5001`,
+  API_URL:`http://172.16.1.21:5002`,
+  //API_URL:`https://localhost:5001`,
   API_NAME: `X-API`,
   API_KEY: `OkQJ5vSEgyFsQzpAzYttBVuCHZAg3mTdRViy4WZByI4qR`,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  API_URL:`https://localhost:5001`,
+  API_URL:`http://172.16.1.21:5002`,
+  //API_URL:`https://localhost:5001`,
   API_NAME: `X-API`,
   API_KEY: `OkQJ5vSEgyFsQzpAzYttBVuCHZAg3mTdRViy4WZByI4qR`,
 };
